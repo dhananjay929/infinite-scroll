7766612c92e86d7c0e36e83192c29026ca6cf7f3
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-04-11 12:04:50 +0530

    infinte-scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import FormState from "./context/FormState";
+import Layout from "./components/condition/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormState>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Layout />} />
+        </Routes>
+      </Router>
+    </FormState>
   );
 }
 
